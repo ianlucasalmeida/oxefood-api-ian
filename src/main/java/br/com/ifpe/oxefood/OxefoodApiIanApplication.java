b22afeb6f0f7bf1fe5982ad18a7de22f86f106f5
aluno <aluno@ifpe>
2026-02-11 20:48:41 -0300

    enviando arquivos do projeto de Web4

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiIanApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiIanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiIanApplication.class, args);
+	}
+
+}
