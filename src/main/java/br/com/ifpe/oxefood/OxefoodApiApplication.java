ba7308fc972139d2dc7be573554688299ebeb39e
ianlucasalmeida <ilas.ianalmeida@gmail.com>
2026-02-25 20:33:29 -0300

    Primeiras interações de criação de clientes e outras entidades

--- src/main/java/br/com/ifpe/oxefood/OxefoodApiApplication.java
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiApplication.java
@@ -2,12 +2,15 @@ package br.com.ifpe.oxefood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-public class OxefoodApiIanApplication {
+@EnableJpaAuditing
+public class OxefoodApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OxefoodApiIanApplication.class, args);
+		SpringApplication.run(OxefoodApiApplication.class, args);
 	}
 
 }
+
