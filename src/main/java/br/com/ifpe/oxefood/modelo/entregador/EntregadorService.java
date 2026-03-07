160bb1efc41abeb37d0089f0f8d5910ca4578b06
ianlucasalmeida <ilas.ianalmeida@gmail.com>
2026-03-06 22:39:18 -0300

    feat: adicionando novas funcionalidades no sistema Oxefood em sua API

--- src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -1,5 +1,7 @@
 package br.com.ifpe.oxefood.modelo.entregador;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +16,34 @@ public class EntregadorService {
     @Transactional
     public Entregador save(Entregador entregador) {
 
-        // Como a entidade herda de EntidadeNegocio, precisamos setar o habilitado como true antes de salvar
+        // Como a entidade herda de EntidadeNegocio, precisamos setar o habilitado como
+        // true antes de salvar
         entregador.setHabilitado(Boolean.TRUE);
 
         return repository.save(entregador);
     }
+
+    public List<Entregador> listarTodos() {
+
+        return repository.findAll();
+    }
+
+    public Entregador obterPorID(Long id) {
+
+        return repository.findById(id).get();
+    }
+
+    @Transactional
+    public void update(Long id, Entregador entregadorAlterado) {
+
+        Entregador entregador = repository.findById(id).get();
+        entregador.setNome(entregadorAlterado.getNome());
+        entregador.setDataNascimento(entregadorAlterado.getDataNascimento());
+        entregador.setCpf(entregadorAlterado.getCpf());
+        entregador.setFoneCelular(entregadorAlterado.getFoneCelular());
+        entregador.setFoneFixo(entregadorAlterado.getFoneFixo());
+
+        repository.save(entregador);
+    }
+
 }
--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -1,5 +1,7 @@
 package br.com.ifpe.oxefood.modelo.produto;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +16,35 @@ public class ProdutoService {
     @Transactional
     public Produto save(Produto produto) {
 
-        // Como a nossa entidade Produto herda de EntidadeAuditavel/EntidadeNegocio, 
+        // Como a nossa entidade Produto herda de EntidadeAuditavel/EntidadeNegocio,
         // precisamos garantir que ela seja salva como "habilitada" por padrão.
         produto.setHabilitado(Boolean.TRUE);
 
         return repository.save(produto);
     }
+
+    public List<Produto> listarTodos() {
+
+        return repository.findAll();
+    }
+
+    public Produto obterPorID(Long id) {
+
+        return repository.findById(id).get();
+    }
+
+    @Transactional
+    public void update(Long id, Produto produtoAlterado) {
+        
+        Produto produto = repository.findById(id).get();
+        
+        produto.setTitulo(produtoAlterado.getTitulo());
+        produto.setDescricao(produtoAlterado.getDescricao());
+        produto.setValor(produtoAlterado.getValor());
+        produto.setTempoEntregaMinimo(produtoAlterado.getTempoEntregaMinimo());
+        produto.setTempoEntregaMaximo(produtoAlterado.getTempoEntregaMaximo());
+
+        repository.save(produto);
+    }
+
 }
